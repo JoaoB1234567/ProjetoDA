9b70be8d7a0468bad531e11c3dd356743fcc9212
Luís Ribeiro <luismjribeiro@sapo.pt>
2026-06-03 09:45:38 +0100

    Adicionei botões ao Form principal que vão levar a cada uma das vistas

--- ProjetoDA/Views/FormListaCompras.Designer.cs
+++ ProjetoDA/Views/FormListaCompras.Designer.cs
@@ -33,15 +33,21 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(997, 150);
+            this.button1.Location = new System.Drawing.Point(975, 510);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(173, 54);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Logout";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -55,11 +61,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(997, 245);
+            this.button3.Location = new System.Drawing.Point(260, 510);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(173, 54);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Gerir Tipos de Artigo";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -75,16 +81,76 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(130, 150);
+            this.listBox1.Location = new System.Drawing.Point(25, 84);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(802, 356);
             this.listBox1.TabIndex = 4;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(25, 510);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(173, 54);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Gerir Artigos";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(975, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(173, 54);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Gerir Orçamento";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(975, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(173, 54);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "Planear Compras";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(975, 297);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(173, 54);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "Criar e Editar Compras";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(975, 84);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(173, 54);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "Modo Compra";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(496, 510);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(173, 54);
+            this.button10.TabIndex = 10;
+            this.button10.Text = "Estatisticas";
+            this.button10.UseVisualStyleBackColor = true;
             // 
             // FormListaCompras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1441, 681);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -103,5 +169,11 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
     }
 }
