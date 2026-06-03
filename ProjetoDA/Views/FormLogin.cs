329e15271d9785b81c1c4c32ac4a010616186bbd
THOMASS47 <63131669+THOMASS47@users.noreply.github.com>
2026-06-03 11:18:08 +0100

    arranjar login

--- ProjetoDA/Views/FormLogin.cs
+++ ProjetoDA/Views/FormLogin.cs
@@ -1,43 +1,35 @@
-﻿using System;
+﻿using ProjetoDA.Controllers;
+using System;
 using System.Windows.Forms;
-using ProjetoDA.Controllers;
 
-namespace ProjetoDA.Views
-{
-    public partial class FormLogin : Form
-    {
+namespace ProjetoDA.Views {
+    public partial class FormLogin : Form {
         private LoginController _loginController;
 
-        public FormLogin()
-        {
+        public FormLogin() {
             InitializeComponent();
             _loginController = new LoginController();
 
             txtPassword.PasswordChar = '*';
         }
 
-        private void btnEntrar_Click(object sender, EventArgs e)
-        {
+        private void btnEntrar_Click(object sender, EventArgs e) {
             string username = txtUsername.Text;
             string password = txtPassword.Text;
 
-            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
-            {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password)) {
                 MessageBox.Show("Por favor, preencha todos os campos.", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 return;
             }
 
-            if (_loginController.ValidarLogin(username, password))
-            {
+            if (_loginController.ValidarLogin(username, password)) {
                 MessageBox.Show($"Bem-vindo, {SessaoGlobal.UtilizadorLogadoNome}!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
                 FormListaCompras principal = new FormListaCompras();
                 principal.Show();
 
                 this.Hide();
-            }
-            else
-            {
+            } else {
                 MessageBox.Show("Credenciais erradas!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 txtPassword.Clear();
                 txtPassword.Focus();
