56ede317392f0b9e90465594acd3a9aa9fdd01a4
THOMASS47 <63131669+THOMASS47@users.noreply.github.com>
2026-06-03 11:28:18 +0100

    add logout

--- ProjetoDA/Views/FormListaCompras.Designer.cs
+++ ProjetoDA/Views/FormListaCompras.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
@@ -41,14 +41,15 @@
             this.button10 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnSair
             // 
-            this.button1.Location = new System.Drawing.Point(975, 510);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 54);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Logout";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnSair.Location = new System.Drawing.Point(975, 510);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(173, 54);
+            this.btnSair.TabIndex = 0;
+            this.btnSair.Text = "Logout";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
             // 
             // button2
             // 
@@ -155,7 +156,7 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSair);
             this.Name = "FormListaCompras";
             this.Text = "FormListaCompras";
             this.ResumeLayout(false);
@@ -164,7 +165,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnSair;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
